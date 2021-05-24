e14ef7b5a0a949e0822a591deb1d18d79dc841b8
bhavanam77 <bhavanahm96@gmail.com>
2021-05-24 01:03:40 +0000

    added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementsById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var year = new Date(). getFullYear()+parseInt(years);
 }
+    function updateRate()
+    {
+        var rateval = document.getElementsById("rate").value;
+        document.getElemntsById("rate_val").innerText=rateval;
+    }
+
+
+        
         
